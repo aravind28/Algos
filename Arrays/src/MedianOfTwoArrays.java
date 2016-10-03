b0a15bd8730d3f6fa5fa08e3b120dd5aeecc469f
Aravindh <eswaran.a@husky.neu.edu>
2016-10-03 12:16:57 -0700

    median of two arrays

--- Arrays/src/MedianOfTwoArrays.java
+++ Arrays/src/MedianOfTwoArrays.java
@@ -1,16 +1,16 @@
 class MedianOfTwoArrays{
-    public static float findMedian(int[] arr1, int arr1Start, int arr1End, int[] arr2,
-                                 int arr2Start, int arr2End){
+    public static float findMedian(int[] arr1, int arr1Start, int arr1End,
+                                   int[] arr2, int arr2Start, int arr2End){
         float result;
         if(arr1Start == arr1End){
-            result =  (arr1[arr1Start] + arr2[arr2Start]) /2;
+            result = (arr1[arr1Start] + arr2[arr2Start]) / 2;
             return result;
         }
 
-        else if((arr1End - arr1Start) == 1){
-            float m1 = Math.min(arr1[arr1Start], arr2[arr2Start]);
-            float m2 = Math.max(arr1[arr1End], arr2[arr2End]);
-            result = (m1 + m2)/2;
+        else if(((arr1End - arr1Start) == 1)){
+            float m1 = Math.max(arr1[arr1Start], arr2[arr2Start]);
+            float m2 = Math.min(arr1[arr1End], arr2[arr2End]);
+            result = (m1 + m2) / 2;
             return result;
         }
         int medianIndexArr1 = arr1Start + ((arr1End - arr1Start) / 2);
@@ -28,8 +28,8 @@ class MedianOfTwoArrays{
     }
 
     public static void main(String[] args) {
-        int[] arr1 = new int[]{2, 3, 4};
-        int[] arr2 = new int[]{5,6, 7};
+        int[] arr1 = new int[]{1, 12, 15, 26, 38};
+        int[] arr2 = new int[]{2, 13, 17, 30, 45};
         float median = findMedian(arr1, 0, arr1.length - 1, arr2, 0, arr2.length - 1);
         System.out.println("Median: " + median);
     }
