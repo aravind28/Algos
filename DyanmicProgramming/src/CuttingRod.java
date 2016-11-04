3d0a19d69088184237e526b1e50530165e3f474d
Aravindh <eswaran.a@husky.neu.edu>
2016-11-03 19:57:13 -0700

    Matrix Multiplication

--- DyanmicProgramming/src/CuttingRod.java
+++ DyanmicProgramming/src/CuttingRod.java
@@ -33,6 +33,48 @@ public class CuttingRod {
         return result[target];
     }
 
+    public static int findUsingMemoization(int[] cuttingLength, int[] price, int target){
+        int[] resultArr = new int[target + 1];
+        int targetSize = resultArr.length;
+        if(target <= 0){
+            return 0;
+        }
+        for(int i = 0; i < targetSize; i++){
+            resultArr[i] = Integer.MIN_VALUE;
+        }
+        return findUsingMemoizationHelper(price, resultArr, target);
+    }
+
+    public static int findUsingMemoizationHelper(int[] price, int[] resultArr, int targetSize){
+        int q = Integer.MIN_VALUE;
+        if(resultArr[targetSize] >= 0){
+            return resultArr[targetSize];
+        } else if(targetSize == 0){
+            q = 0;
+        }
+        else{
+            for(int i = 1; i <= targetSize; i++){
+                q = findMax(q, price[targetSize], findUsingMemoizationHelper(price, resultArr, targetSize - i));
+            }
+        }
+        resultArr[targetSize] = q;
+        return resultArr[targetSize];
+    }
+
+    private static int findMax(int a, int b, int c){
+        if(a > b){
+            if(a > c){
+                return c;
+            } else{
+                return c;
+            }
+        } else if (b > c){
+            return b;
+        } else {
+            return c;
+        }
+    }
+
 
     public static void main(String[] args){
         int[] cuttingLength = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
@@ -41,9 +83,11 @@ public class CuttingRod {
 
         // Ans 10
         System.out.println();
-        System.out.print("Maximum value for target " + target + " is (method 1): ");
-        System.out.println(findMaxValue(cuttingLength, price, target));
-        System.out.print("Maximum value for target " + target + " is (method 2): ");
-        System.out.println(findMaxValue2(cuttingLength, price, target));
+//        System.out.print("Maximum value for target " + target + " is (method 1): ");
+//        System.out.println(findMaxValue(cuttingLength, price, target));
+//        System.out.print("Maximum value for target " + target + " is (method 2): ");
+//        System.out.println(findMaxValue2(cuttingLength, price, target));
+        System.out.print("Maximum value for target " + target + " is (memoization): ");
+        System.out.println(findUsingMemoization(cuttingLength, price, target));
     }
 }
---
+++ DyanmicProgramming/src/LongestCommonSubsequence.java
@@ -0,0 +1,41 @@
+
+public class LongestCommonSubsequence {
+    public static void findLCS(int[] arr1, int [] arr2){
+        int rowSize = arr2.length;
+        int colSize = arr1.length;
+        int[][] result = new int[rowSize + 1][colSize + 1];
+        for(int row = 0; row < result.length; row++){
+            result[row][0] = 0;
+        }
+        for(int col = 0; col < result[0].length; col++){
+            result[0][col] = 0;
+        }
+        for(int row = 1; row < result.length; row ++){
+            for(int col = 1; col < result[0].length; col++){
+                if(arr2[row - 1] == arr1[col - 1]){
+                    result[row][col] = 1 + result[row - 1][col - 1];
+                }
+                else{
+                    result[row][col] = Math.max(result[row][col - 1], result[row-1][col]);
+                }
+            }
+        }
+        printMatrix(result);
+    }
+
+    private static void printMatrix(int[][] resultMatrix){
+        for(int i = 0; i < resultMatrix.length; i++){
+            System.out.println();
+            for(int j = 0; j < resultMatrix[0].length; j++){
+                System.out.print(" " + resultMatrix[i][j]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+//        char arr1[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+//        char arr2[] = {'a', 'c', 'b', 'c', 'f'};
+        int arr1[] = {1, 0, 0, 1, 0, 1, 0, 1};
+        int arr2[] = {0, 1, 0, 1, 1, 0, 1, 1, 0};
+        findLCS(arr1, arr2);
+    }
+}
